0f346fd2a060f59dfcfed6ce0a4542b26c4eb538
edwinisnow <edwin04@gmail.com>
2020-02-21 05:44:46 +0000

    User, Company classes added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello World')
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+console.log("Log: -----> : user", user)
+
+const company = new Company();
+console.log("Log: -----> : company", company)
