d44bf855389b4c2ff2f21e0fe79ff4778b72a5e5
edwinisnow <edwin04@gmail.com>
2020-02-21 06:59:39 +0000

    custom map class

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { User } from './User';
 import { Company } from './Company';
 
-const user = new User();
-console.log("Log: -----> : user", user)
+// const user = new User();
+// console.log("Log: -----> : user", user)
 
-const company = new Company();
-console.log("Log: -----> : company", company)
+// const company = new Company();
+// console.log("Log: -----> : company", company)
+
+
+
